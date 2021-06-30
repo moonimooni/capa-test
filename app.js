a6d4ddffd7053b9cbaefa703b3736d519bffc3e4
moonimooni <munhw1408@gmail.com>
2021-06-30 15:20:33 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.listen(8080, () => {
+  console.log("port 8080 on");
+});
