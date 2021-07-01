e7de581d7b22a671acc0e3931516d5765269868f
moonimooni <munhw1408@gmail.com>
2021-07-01 16:39:57 +0900

    [Add]get stores in radius

--- services/store.js
+++ services/store.js
@@ -1,4 +1,5 @@
 const { createError } = require("../libs/error");
+const postcodeIoCalls = require("./prefix/postcode_io");
 
 /**
  *
@@ -16,6 +17,39 @@ exports.getStores = (offset, limit, storesList) => {
   } else {
     return storesList;
   }
+};
+
+/**
+ *
+ * @param {string} postcode
+ * @param {number} radius
+ * @param {storesList} array
+ */
+exports.getStoresInRadiusByPostcode = async (postcode, radius, storesList) => {
+  const { longitude, latitude } = await postcodeIoCalls.getPostCodeInformation(
+    postcode
+  );
+  const addresses = await postcodeIoCalls.getNearestPostcodesInformation(
+    longitude,
+    latitude,
+    radius
+  );
+  addresses.map((address) => {
+    return {
+      longitude: address.longitude,
+      latitude: address.latitude,
+      postcode: address.postcode,
+      name: address.parliamentary_constituency,
+    };
+  });
+  addresses.sort((a, b) => {
+    return a.latitude - b.latitude;
+  });
+  return storesList.filter((store) => {
+    return addresses.includes((address) => {
+      return address.postcode === store.postcode;
+    });
+  });
 };
 
 /**
